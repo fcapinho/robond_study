1fef0eaf737efe0389082bc487652ad88db8db4f
Fernando Pinho <fpinho@gmail.com>
2020-10-31 09:03:44 -0700

    Update BFS solution

--- path_planning/bfs/src/main.cpp
+++ path_planning/bfs/src/main.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <vector>
 #include <algorithm>
+#include <array>
 
 using namespace std;
 
@@ -48,11 +49,62 @@ void print2DVector(T Vec)
     }
 }
 
+void printTriplet(array<int, 3> triplet)
+{
+    cout << "[ ";
+    for(const auto& t: triplet) {
+        cout << t << ' ';
+    }
+    cout << "]" << endl;
+}
+
 /*#### TODO: Code the search function which will generate the expansion list ####*/
 // You are only required to print the final triplet values
 void search(Map map, Planner planner)
 {
-    
+    array<int, 3> cell_picked;
+    vector<array<int, 3>> open_list = { {0, planner.start[0], planner.start[1]} };
+    int expansion = 0;
+    vector<array<int, 2>> visited_cells = { {planner.start[0], planner.start[1]} };
+
+    while (!open_list.empty()) {
+        // cout << "Expansion #: " << expansion++ << endl;
+
+        // Pick cell
+        cell_picked = open_list.back();
+        open_list.pop_back();
+
+        // cout << "Next cell: "; printTriplet(cell_picked);
+        
+        // Check if reached the goal
+        if ( (cell_picked[1] == planner.goal[0]) && (cell_picked[2] == planner.goal[1])) {
+            printTriplet(cell_picked);
+            return;
+        }
+
+        // Expand the search
+        for (const auto& m: planner.movements) {
+            int new_x = cell_picked[1] + m[0];
+            int new_y = cell_picked[2] + m[1];
+            // Check for valid coordinates
+            if ( (new_x >= 0) && (new_x < map.mapHeight) && (new_y >= 0) && (new_y < map.mapWidth) ) {
+                // Check for visited cell
+                vector<array<int, 2>>::iterator it;
+                array<int, 2> new_cell = { new_x, new_y };
+                it = find (visited_cells.begin(), visited_cells.end(), new_cell );
+                if (it == visited_cells.end()) {
+                    // Check for empty cell
+                    if ( map.grid[new_x][new_y] == 0) {
+                        array<int, 3> new_item = { cell_picked[0] + planner.cost, new_x, new_y };
+                        open_list.insert(open_list.begin(), new_item);
+                        visited_cells.push_back(new_cell);
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "Roadblock" << endl;
 }
 
 int main()
